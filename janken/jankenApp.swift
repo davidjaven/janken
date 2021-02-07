ddd685ead90f260713a44123080c671818809c72
javen <davidjaven@gmail.com>
2021-02-07 20:12:18 +0900

    Initial Commit

---
+++ janken/jankenApp.swift
@@ -0,0 +1,17 @@
+//
+//  jankenApp.swift
+//  janken
+//
+//  Created by 世家 劉 on 2021/02/07.
+//
+
+import SwiftUI
+
+@main
+struct jankenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
